7801d23cd391e0c191073768ddcfc9af002d5d16
SkyFi Geek <45924209+mobileskyfi@users.noreply.github.com>
2024-05-26 19:05:29 -0700

    try oas2 convert in ci

--- raml2oas.cjs
+++ raml2oas.cjs
@@ -2,20 +2,28 @@ const wap = require('webapi-parser').WebApiParser
 const fs = require("fs")
 
 async function main() {
+    console.log("Parsing YAML RAML file: ", process.argv[2])
     const raml = fs.readFileSync(`${__dirname}/${process.argv[2]}`, "utf-8")
     const model = await wap.raml10.parse(raml)
 
+    console.log("Generation OAS 2.0")
     await wap.oas20.generateFile(model, `file://${__dirname}/ros-oas20.json`)
-    const oas20 = fs.readFileSync(`${__dirname}/ros-oas20.json`, "utf-8")
-    const oas20model = await wap.oas20.parse(oas20)
+    console.log("Validating OAS 2.0")
+    const oas20model = await wap.oas20.parse(`file://${__dirname}/ros-oas20.json`)
     const oas20validate = await wap.oas20.validate(oas20model)
-    console.log('OAS 2.0 validation errors:', oas20validate.toString())
+    console.log('OAS 2.0 validation', oas20validate.toString())
 
+    // OAS 3.0 DOES NOT VALIDATE
+    // while it does import to postman... there are 3K+ validation errors
+    // comment out since OAS 2.0 should be fine
+    /*
+    console.log("Generation OAS 3.0")
     await wap.oas30.generateFile(model, `file://${__dirname}/ros-oas30.json`)
-    const oas30 = fs.readFileSync(`${__dirname}/ros-oas30.json`, "utf-8")
-    const oas30model = await wap.oas30.parse(oas30)
+    console.log("Validating OAS 3.0")
+    const oas30model = await wap.oas30.parse(`file://${__dirname}/ros-oas30.json`)
     const oas30validate = await wap.oas30.validate(oas30model)
-    console.log('OAS 3.0 validation errors:', oas30validate.toString())
+    console.log('OAS 3.0 validation:', oas30validate.toString())
+    */
 }
 
 
